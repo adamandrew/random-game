93d814c840cd43154ef03d6b2e8ead913c0ec5b4
Andrew Lee <email": null>
2016-06-07 17:58:23 -0700

    copied and paste basic skeleton

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,58 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  // Matter.js module aliases
+  var Engine = Matter.Engine,
+  World = Matter.World,
+  Body = Matter.Body,
+  Bodies = Matter.Bodies,
+  Common = Matter.Common,
+  Constraint = Matter.Constraint,
+  Composites = Matter.Composites,
+  MouseConstraint = Matter.MouseConstraint;
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // create a Matter.js engine
+  var engine = Engine.create(document.body, {
+    render: {
+      options: {
+        wireframes: true,
+        showAngleIndicator: true
+      }
+    }
+  });
+
+  //add a mouse-controlled constraint
+  var mouseConstraint = MouseConstraint.create(engine);
+  World.add(engine.world, mouseConstraint);
+
+  // create a load of circle bodies
+  // var stack = Composites.stack(250, 5, 12, 20, 0, 0, function(x, y, column, row) {
+  //   return Bodies.circle(x, y, Common.random(5, 15), { friction: .001, restitution: .1, density: 5.5 });
+  // });
+  var rock = Bodies.rectangle(200, 450, 50, 20, { friction: 0.05, chamfer: { radius: 10 }})
+      anchor = { x: 200, y: 450}
+
+  var elastic = Constraint.create({
+      pointA: anchor,
+      bodyB: rock,
+      stiffness: 0.05,
+      render: {
+          lineWidth: 5,
+          strokeStyle: '#dfa417'
+      }
+  });
+
+  // add boundaries
+  var offset = 5;
+  World.add(engine.world, [
+    Bodies.rectangle(400, -offset, 800 + 2 * offset, 50, { isStatic: true }),
+    Bodies.rectangle(400, 600 + offset, 800 + 2 * offset, 50, { isStatic: true }),
+    Bodies.rectangle(800 + offset, 300, 50, 600 + 2 * offset, { isStatic: true }),
+    Bodies.rectangle(-offset, 300, 50, 600 + 2 * offset, { isStatic: true })
+    ]);
+
+  // add all of the bodies to the world
+  // World.add(engine.world, stack);
+  World.add(engine.world, [rock, elastic]);
+
+  // run the engine
+  Engine.run(engine);
 });
